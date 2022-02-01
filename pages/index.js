f41d300b080205765e3d0260b4d5289a889717d4
martinbolsnes <martinbolsnes@icloud.com>
2022-02-01 13:18:23 +0100

    imported global styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-import styles from '../styles/Home.module.css';
+import HeroHeading from '../components/hero/heroHeading';
+import HeroHeadingSpan from '../components/hero/heroHeadingSpan';
+import Container from '../components/container/container';
+import HeroParagraph from '../components/hero/heroParagraph';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <p>Hi there, I'm</p>
+        <HeroHeading>
+          Martin Bolsønes
+          <br />
+          <HeroHeadingSpan>Frontend developer</HeroHeadingSpan>
+        </HeroHeading>
+        <HeroParagraph>
+          Frontend devlopment student in Oslo. Enjoy creating modern web
+          applications and is eager to learn and emprove in both design and
+          programming.
+        </HeroParagraph>
+      </Container>
+    </div>
+  );
 }
