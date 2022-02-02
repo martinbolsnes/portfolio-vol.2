297b6ecf1f9cdccc7d703d5a59e35e9e2aaa1adf
martinbolsnes <martinbolsnes@icloud.com>
2022-02-02 15:27:39 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <Main>
           <HeroWrapper>
             <HeroContainer>
-              <HeroParagraph>Hi there, Im</HeroParagraph>
+              <HeroParagraph>Hi there, I</HeroParagraph>
               <HeroHeading>
                 Martin Bolsønes
                 <br />
