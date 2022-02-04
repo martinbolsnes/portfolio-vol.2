4e32a464fb48afbd82a6d55291300020b65ce844
martinbolsnes <martinbolsnes@icloud.com>
2022-02-04 16:04:52 +0100

    created about section

---
+++ components/about/AboutSection.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AboutSection = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--color-primary);
+`;
+
+export default AboutSection;
---
+++ components/about/AboutWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AboutWrapper = styled.div`
+  max-width: 1440px;
+  display: flex;
+  flex-direction: column;
+  padding-right: var(--size-xl);
+  padding-left: var(--size-xl);
+  margin-bottom: var(--size-xl);
+`;
+
+export default AboutWrapper;
