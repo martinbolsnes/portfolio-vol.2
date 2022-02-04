6986c5355f96fd7fa92e9975b582843df91f7ced
martinbolsnes <martinbolsnes@icloud.com>
2022-02-04 12:10:12 +0100

    animation icons

--- components/icons/GithubIcon.js
+++ components/icons/GithubIcon.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import media from 'styled-media-query';
 import { GitHub } from 'react-feather';
+import rotateIcons from '../RotateIcons';
 
 const GithubIcon = styled(GitHub)`
   cursor: pointer;
 
   :hover {
     stroke: var(--color-secondary);
+    display: inline-block;
+    animation ${rotateIcons} 0.2s linear; 
   }
 
   ${media.lessThan('medium')`
