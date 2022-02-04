ccf727df039acf3dfb8cdf506f66bee4e3da174c
martinbolsnes <martinbolsnes@icloud.com>
2022-02-04 16:05:13 +0100

    h2 comp

---
+++ components/headings/H2.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+  font-size: var(--font-size-lg);
+  font-weight: var(--font-weight-black);
+  margin-bottom: -6px;
+  margin-top: var(--size-xl);
+`;
+
+export default H2;
