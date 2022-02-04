5ba98fab15f390f9eaaac6bf75f88a6224ff9804
martinbolsnes <martinbolsnes@icloud.com>
2022-02-04 12:08:58 +0100

    rotate comp

---
+++ components/RotateIcons.js
@@ -0,0 +1,13 @@
+import { keyframes } from 'styled-components';
+
+const rotateIcons = keyframes`
+from {
+    transform: rotate(0deg);
+}
+to{
+    transform: rotate(20deg);
+}
+
+`;
+
+export default rotateIcons;
