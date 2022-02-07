de06f5bacaf35dde437f1044894d5f91823e921f
martinbolsnes <martinbolsnes@icloud.com>
2022-02-07 14:10:31 +0100

    h3 comp

---
+++ components/headings/H3.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+const H3 = styled.h3`
+  font-size: var(--font-size-md);
+  font-weight: var(--font-weight-bold);
+  margin-bottom: var(--size-md);
+`;
+
+export default H3;
