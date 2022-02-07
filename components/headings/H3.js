f99024650819429c5a3ecef7b1a5db53c0cb7ea0
martinbolsnes <martinbolsnes@icloud.com>
2022-02-07 15:02:37 +0100

    changed font

--- components/headings/H3.js
+++ components/headings/H3.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import media from 'styled-media-query';
 
 const H3 = styled.h3`
+  font-family: var(--font-body);
   font-size: var(--font-size-md);
   font-weight: var(--font-weight-bold);
   margin-bottom: var(--size-md);
