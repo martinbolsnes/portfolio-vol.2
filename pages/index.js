4e8ac685c530277adf8c4e6dae413344b2a23766
martinbolsnes <martinbolsnes@icloud.com>
2022-06-07 14:08:00 +0200

    added width + margin classes

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <Header>
           <NavBar />
         </Header>
-        <Main>
+        <Main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
           <Hero />
           <About />
           <Projects />
