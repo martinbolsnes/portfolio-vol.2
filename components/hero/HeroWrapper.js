cf340e5bfe6589d7560945381fa6972761890517
martinbolsnes <martinbolsnes@icloud.com>
2022-06-07 14:07:28 +0200

    edit padding

--- components/hero/HeroWrapper.js
+++ components/hero/HeroWrapper.js
@@ -8,17 +8,9 @@ const HeroWrapper = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding-right: var(--size-xl);
-  padding-left: var(--size-xl);
 
   ${media.lessThan('medium')`
-  flex-direction: column;
-  padding-right: var(--size-sm);
-  padding-left: var(--size-sm);`}
-
-  ${media.lessThan('small')`
-  padding-right: var(--size-xs);
-  padding-left: var(--size-xs);`}
+  flex-direction: column;`}
 `;
 
 export default HeroWrapper;
