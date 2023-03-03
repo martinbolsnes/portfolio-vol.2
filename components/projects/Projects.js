8056589492814cbc821496b0ee23c1174ab2ae3c
martinbolsnes <martinbolsnes@icloud.com>
2023-03-03 14:15:27 +0100

    add new project

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -3,7 +3,7 @@ import ProjectsWrapper from './ProjectsWrapper';
 import ProjectOne from './ProjectOne';
 import ProjectTwo from './ProjectTwo';
 import ProjectThree from './projectThree';
-import ProjectFour from './projectFour';
+import ProjectFive from './projectFive';
 import styles from '../../styles/Home.module.css';
 import H2 from '../headings/H2';
 import Fade from 'react-reveal/Fade';
@@ -16,6 +16,15 @@ export default function Projects() {
           <Fade bottom>
             <H2>Recent Projects</H2>
             <span className={styles.orangeLine}></span>
+          </Fade>
+          <Fade bottom>
+            <ProjectFive />
+          </Fade>
+          <Fade bottom>
+            <span
+              className={styles.orangeLine}
+              style={{ width: '100%' }}
+            ></span>
           </Fade>
           <Fade bottom>
             <ProjectTwo />
